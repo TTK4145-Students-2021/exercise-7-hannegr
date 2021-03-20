e6da03c28609aee3ab8ce6309d9227495564174d
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-03-20 18:47:16 +0100

    go a long way, but slave commandowindo starts counting before it should

---
+++ phoenix/phoenix.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func openPrimaryAndBackupFile(i int) {
+	if i == 0{
+	    os.Create("primary.txt")
+	    addNumber(i)
+	} else{
+		 os.Rename("backup.txt", "primary.txt")
+	}
+	os.Open("primary.txt")
+	os.Create("backup.txt")
+}
+
+ func addNumber(b int){
+ 	primary,_ := os.OpenFile("primary.txt",os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0744)
+ 	 _, err := primary.WriteString(fmt.Sprintf("%d\n", b+1))
+ 	 fmt.Println(b+1)
+ 	 if err != nil{
+ 	 	log.Fatal(err)
+ 	 }
+ 	 time.Sleep(time.Second)
+ }
+
+ func addToBackupAndGetI() int{
+ 	_, err := os.Stat("primary.txt")
+ 	    if os.IsNotExist(err) {
+ 	       	 openPrimaryAndBackupFile(0)
+ 	   }
+ 	data, err := ioutil.ReadFile("primary.txt")
+ 	if err != nil {
+ 		log.Panicf("failed reading data from file: %s", err)
+ 	}
+ 	numbers := strings.Split(string(data), "\n")
+ 	num,_ := strconv.Atoi(numbers[len(numbers)-2])
+ 	backup,_ := os.OpenFile("backup.txt",os.O_WRONLY|os.O_CREATE, 0744)
+ 	backup.WriteString(fmt.Sprintf("%d\n", num))
+ 	if err != nil{
+ 		 log.Fatal(err)
+ 	}
+ 	return num
+ }
+ 
+
+ func checkTimeOut(a1 int, a2 int)bool{
+ 	if a1 == a2{
+ 		return true
+	}
+	return false
+ }
+
+func main() {
+	cmd := exec.Command("cmd", "/C", "start", "cmd", "go", "run", "main.go") //the second cmd can be changed with powershell
+	err := cmd.Run()
+	if err != nil { //getNewPrimaryandBackup()
+		log.Fatal(err)
+
+	}
+	//finn på noe sjekktimeout == false greier
+	for {
+		//hvis checkTimeout == false liksom
+		addToBackupAndGetI()
+		openPrimaryAndBackupFile(addToBackupAndGetI())
+		addNumber(addToBackupAndGetI())
+	}
+
+}
